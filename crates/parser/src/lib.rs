f0cfb23482637411e49da1617628ebe98e50b48d
mahdifrmz <faramarzpour98@gmail.com>
2023-06-08 19:11:56 +0430

    feat: command parser

--- crates/parser/src/lib.rs
+++ crates/parser/src/lib.rs
@@ -1,9 +1,150 @@
-use server::Output;
+use server::Input;
 
-struct Parser {}
+pub struct Parser {
+    state: ReplState,
+    fields: Vec<String>,
+    pointer: usize,
+    buffer: Option<Token>,
+}
+
+#[derive(Clone, PartialEq, Eq)]
+enum Token {
+    Int(usize),
+    String(String),
+    Tcp,
+    Udp,
+    True,
+    False,
+    EOF,
+}
+
+pub enum Error {
+    Empty,
+    UnknowCommand,
+    UnexpectedEnd,
+    InvalidParam(usize),
+    HostNotSpecified,
+    InvalidPort(usize),
+}
+
+pub struct ReplState {
+    pub host: Option<String>,
+}
 
 impl Parser {
-    fn parse(command: String) -> Output {
-        Output::Term
+    fn read(&mut self) -> Token {
+        let field = if let Some(s) = self.fields.first().cloned() {
+            self.fields.remove(0);
+            s
+        } else {
+            return Token::EOF;
+        };
+        self.pointer += 1;
+        match field.parse::<usize>() {
+            Ok(val) => Token::Int(val),
+            Err(_) => match field.to_lowercase().as_str() {
+                "true" => Token::True,
+                "false" => Token::False,
+                "tcp" => Token::Tcp,
+                "udp" => Token::Udp,
+                _ => Token::String(field),
+            },
+        }
+    }
+    fn peek(&mut self) -> Token {
+        if let Some(buf) = self.buffer.clone() {
+            buf
+        } else {
+            let token = self.read();
+            self.buffer = Some(token.clone());
+            token
+        }
+    }
+    fn next(&mut self) -> Token {
+        if let Some(buf) = self.buffer.clone() {
+            self.buffer = None;
+            buf
+        } else {
+            self.read()
+        }
+    }
+    fn parse_number(&mut self) -> Result<usize, Error> {
+        let token = self.next();
+        match token {
+            Token::Int(value) => Ok(value),
+            _ => Err(Error::InvalidParam(self.pointer)),
+        }
+    }
+    fn parse_string(&mut self) -> Result<String, Error> {
+        let token = self.next();
+        match token {
+            Token::String(value) => Ok(value),
+            _ => Err(Error::InvalidParam(self.pointer)),
+        }
+    }
+    fn parse_config(&mut self) -> Result<Input, Error> {
+        let key = self.next();
+        if let Token::String(key) = key {
+            match key.as_str() {
+                "threads" | "thread" | "t" => Ok(Input::Threads(self.parse_number()?)),
+                _ => Err(Error::InvalidParam(self.pointer)),
+            }
+        } else {
+            Err(Error::InvalidParam(self.pointer))
+        }
+    }
+    fn parse_scan(&mut self) -> Result<Input, Error> {
+        let host = if let Token::String(name) = self.peek() {
+            let name = name;
+            self.next();
+            name
+        } else {
+            self.state.host.clone().ok_or(Error::HostNotSpecified)?
+        };
+        let is_tcp = if self.peek() == Token::Tcp {
+            self.next();
+            true
+        } else if self.peek() == Token::Udp {
+            self.next();
+            false
+        } else {
+            true
+        };
+        let from = self.parse_number()?;
+        let to = if let Token::Int(num) = self.peek() {
+            num
+        } else {
+            from
+        };
+        if from > 0xffff {
+            return Err(Error::InvalidPort(from));
+        }
+        if to > 0xffff {
+            return Err(Error::InvalidPort(to));
+        }
+        Ok(if is_tcp {
+            Input::TcpRange(host, from as u16, to as u16)
+        } else {
+            Input::UdpRange(host, from as u16, to as u16)
+        })
+    }
+    pub fn parse(&mut self, state: ReplState, command: String) -> Result<Input, Error> {
+        self.state = state;
+        let mut command = command.split(' ').map(|s| s.to_owned()).collect::<Vec<_>>();
+        if let Some(name) = command.first().cloned() {
+            command.remove(0);
+            self.fields = command;
+            let output = match name.as_str() {
+                "quit" | "q" | "exit" => Input::End,
+                "cancel" | "c" => Input::Cancel,
+                "resume" | "r" => Input::Cont,
+                "config" | "conf" | "cfg" => self.parse_config()?,
+                "scan" | "s" => self.parse_scan()?,
+                _ => return Err(Error::UnknowCommand),
+            };
+            Ok(output)
+        } else {
+            Err(Error::Empty)
+        }
     }
 }
